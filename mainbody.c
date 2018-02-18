b1c955409586ba49247ecd4cb605620d5ebd33d2
BZYan <35999889+BZYan@users.noreply.github.com>
2018-02-18 18:09:15 -0500

    Add files via upload

--- mainbody.c
+++ mainbody.c
@@ -1,45 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-int storefield(char* line, int column, int** output)
-{
-    const char* cursor;
-    for (cursor = strtok(line, ";"); cursor; cursor = strtok(NULL, ";\n"))
-    {
-        int row=0;
-        int temp=atoi(cursor);
-	output[row][column]=temp; 
-	row++;
-    }
-    return 0;
-}
 
 int 
 main (int argc, char *argv[]){
-	FILE *input= fopen("k.csv","r");
-	
-	char line[128];
-	int value[128][128];
-	double out[128];
-	int cofv,rofv=0;
-	int** ptr=value;
-	while(fgets(line,128,input)!=NULL){
 
-		char* buffer= strdup(line);
-		storefield(buffer,cofv,ptr);		
-		free(buffer);
-		cofv++;
+typedef struct node node;
+
+struct node{
+		char* name;
+		int ocr;
+};
+
+int scomparator (const void* pa, const void* pb){
+	char* a= (const char*) pa;
+	char* b=(const char* ) pb;
+	int leng;
+	if(strlen(a)>strlen(b)) leng=strlen(b); else leng=strlen(a);
+	for (int i=0;i<leng;i++){
+		if (a[i]!=b[i]) return b[i]-a[i];
 	}
-	rofv=sizeof(value)/(cofv*sizeof(int));
-	for(int i=0;i<rofv;i++){
-		for(int j=0;j<cofv;j++){
-			out[i]+=(double)value[i][j];
-		}
-		out[i]=out[i]/(double)cofv;
-		printf("avg of row %d is %f\n",i,out[i]);
-	}
-		
+	if(strlen(a)>strlen(b)) return 1; else return -1;
+}
 	
+int ncomparator (const void *pa, const void *pb){
+	node* a=(node*) pa;
+	node* b=(node*) pb;
+	return (b->ocr - a->ocr);
+}
+
+
+
+	FILE *input= fopen(argv[1],"r");
+	
+	char word[1000][100];
+	char ch;
+	int wordcnt=0;
+	char buffer[100];
+	
+	buffer[0]='\0';
+
+	for( int i=0, j=0; ((ch=getc(input))!=EOF);){	
+		
+		if (! isalnum(ch)) {
+			if(j!=0){
+				
+				//word[i]=malloc(sizeof(char)*strlen(buffer));
+				strcpy(word[i],buffer);
+				i++;  
+				wordcnt++;
+			}
+			j=0;
+			buffer[0]='\0';
+		}else{
+			buffer[j]=tolower(ch);
+			j++;
+		} 
+		
+			buffer[j]='\0'; 
+			
+	}
+
+	if(buffer[0]!='\0') {
+		strcpy(word[wordcnt],buffer);
+		wordcnt++;
+	}
+	
+	
+	
+	
+	
+	
+	
+	for(int i=0; i<wordcnt; i++){
+		printf("%s\n",word[i]);
+		
+	}
+	
+	
+	qsort((void*)word, wordcnt, sizeof(char[100]),scomparator);
+	
+	for(int i=0; i<wordcnt; i++){
+		printf("%s\n",word[i]);
+		
+	}
+
+
+
+
+	node table[wordcnt];	
+	int pop=0;
+
+	int occurcnt=1;
+	int checker=0;
+
+	for (int i=1; i<wordcnt; i++){
+		
+		if (!strcmp(word[checker], word[i])&&(i!=(wordcnt-1))) {
+			occurcnt++;
+		}else{
+			node new= {.name=word[checker], .ocr=occurcnt};
+			
+			table[pop]=new;
+			pop++;
+			checker=i;
+			occurcnt=1;
+		}
+		
+	}
+	qsort(table, pop, sizeof(node),ncomparator);
+
+	for(int i=9; i>=0; i--) {
+		printf ("%d, %s\n",table[i].ocr, table[i].name);
+	}
 	return 0;
 }
